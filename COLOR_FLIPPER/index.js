f6fde6a8b00ef19cef5047c5f76f2df3e26fd850
12anand34 <anand47bansal@gmail.com>
2025-02-28 21:44:03 +0530

    Added random Color fn

--- COLOR_FLIPPER/index.js
+++ COLOR_FLIPPER/index.js
@@ -4,3 +4,9 @@ function setColor(name) {
     body.style.backgroundColor = name;
 }
 
+function randomColor() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    setColor(`rgb(${red},${green},${blue}`);
+}
