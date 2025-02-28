f70c5063793c60a1cf97d53f552d127a72b59a1c
12anand34 <anand47bansal@gmail.com>
2025-03-01 05:11:35 +0530

    Completed to do list

--- TO-DO LIST/index.js
+++ TO-DO LIST/index.js
@@ -1,11 +1,58 @@
 let items = []
 
-function loadItems(){}
+const input = document.getElementById("itemInput")
+const itemsDiv = document.getElementById("items")
+const storageKey = "items"
 
-function renderItems(){}
+function renderItems() {
+    itemsDiv.innerHTML = null;
+    //Object.entries changes array to someting like [0,"item one"],[1,"item two"]..
+    for (const [idx, item] of Object.entries(items)) {
+        const container = document.createElement("div")
+        container.style.marginBottom = "10px"
 
-function saveItems(){}
+        const text = document.createElement("p") // p tags are block element 
+        text.style.display = "inline"
+        text.style.marginRight = "10px"
+        text.textContent = item;
 
-function addItem(){}
+        const button = document.createElement("button")
+        button.textContent = "Done"
+        button.onclick = () => removeItem(idx) //JS calls the removeItem even if button is not pressed, so we wrap it inside another function
+        container.appendChild(text)
+        container.appendChild(button)
 
-function removeItem(){}
+        itemsDiv.appendChild(container)
+    }
+}
+
+function loadItems() {
+    const oldItems = localStorage.getItem(storageKey)
+    if (oldItems) items = JSON.parse(oldItems)
+    renderItems()
+}
+
+function saveItems() {
+    const stringItems = JSON.stringify(items)
+    localStorage.setItem(storageKey, stringItems)
+}
+
+function addItem() {
+    const value = input.value;
+    if (!value) {
+        alert("You cannot add an empty item")
+        return
+    }
+    items.push(value)
+    renderItems();
+    input.value = ""
+    saveItems()
+}
+
+function removeItem(idx) {
+    items.splice(idx, 1)
+    renderItems()
+    saveItems()
+}
+
+document.addEventListener("DOMContentLoaded", loadItems)
